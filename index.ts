b3c1a3cc146bb7c3eb83487a14141363bab38454
endgula <endgula20@gmail.com>
2026-03-08 03:41:00 -0500

    v5

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ const server = Bun.serve({
   port: process.env.PORT ?? 3000,
   async fetch(req) {
     return new Response("Servidor bun-ai-api funcionando en la nueva instancia");
-  },
+  }
 });
 
-console.log(`Servidor activo en http://${server.hostname}:${server.port}`);
+console.log(`Servidor activo en ${server.url}:${server.port}`);
