f6c534bdf303283925593846a534333f71b2b000
endgula <endgula20@gmail.com>
2026-03-08 03:42:44 -0500

    v1

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 const server = Bun.serve({
-  hostname: "0.0.0.0", // ESTA LÍNEA ES OBLIGATORIA PARA AWS
   port: process.env.PORT ?? 3000,
   async fetch(req) {
     return new Response("Servidor bun-ai-api funcionando en la nueva instancia");
