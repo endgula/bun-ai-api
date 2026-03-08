0007e2ed5db9fde9368e16b5fa717834243f2f00
endgula <endgula20@gmail.com>
2026-03-08 03:03:16 -0500

    v2

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+  hostname: "0.0.0.0",
+  port: process.env.PORT ?? 3000,
+  async fetch(req) {
+    return new Response("API de Bun está funcionando correctamente");
+  }
+})
+
+console.log(`Server is running on ${server.url}:${server.port}`);
